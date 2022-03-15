b154c6fb5c16b1cd2bbd1562c3e3054f5af23dac
Sam <swiseman2005@gmail.com>
2022-03-14 20:48:35 -0500

    cleaned useless code and configured buttons

--- client/src/App.js
+++ client/src/App.js
@@ -2,42 +2,42 @@ import React from 'react';
 import style from "./core/styles/login.css"
 
 class App extends React.Component {
-  constructor(props)  {                                                   // Init our constructor and variables.
+  constructor(props)  {                                                     // Init our constructor and variables.
     super(props);
     this.state = {
       APIResponce: '',
       username: '',
       password: ''
     };
-    this.Authenticate = this.Authenticate.bind(this);                     // So we are able to call this from render()'s html.
+    this.Authenticate = this.Authenticate.bind(this);                       // So we are able to call this from render()'s html.
+    this.Register = this.Register.bind(this);  
     }
 
-  Authenticate(event)  {
-    event.preventDefault();                                               // So we don't refresh page on function call.
-    let dataPOST =  {
-      username: this.state.username,
-      password: this.state.password
-    };
-    if(this.state.username != '' && this.state.password != 0)  {
-      fetch('http://localhost:3000/api/v1/login', {
-        method: 'POST',
-        body: JSON.stringify(dataPOST),
-        headers: { 'Content-Type': 'application/json' }
-      })
-      .then((res) => res.json())                                           // Parse responce as HTML.
-      .then((data) => this.handleChange({APIResponce: data.success}))      // Set APIResponce to data.TYPE <-- responce of the testAPI function.
-      .catch(this.handleChange({APIResponce: 'Failed to recive responce.'})); // If failed return an error.
+    Register(event) {                                                       // Todo: add a register function to the API
+      event.preventDefault();
     }
-    else
-      this.handleChange({APIResponce: 'Please enter a username/password'}); // If failed return an error.
-  }
 
-  componentWillMount()  {                                                // This is periodicly run.
-  }
+    async Authenticate(event)  {
+      event.preventDefault();                                               // So we don't refresh page on function call.
+      let dataPOST =  {
+        username: this.state.username,
+        password: this.state.password
+      };
+      if(this.state.username != '' && this.state.password != 0)  {
+        const response = await fetch('http://localhost:3000/api/v1/login', {
+          method: 'POST',
+          body: JSON.stringify(dataPOST),
+          headers: { 'Content-Type': 'application/json' }
+        })
+        .then((res) => res.json())                                           // Parse responce as JSON.
+        .then((data) => this.setState({APIResponce: data.success}))          // Set APIResponce to data.TYPE <-- responce of the api/v1/login function.
+        .catch((error) => this.setState({APIResponce: 'Failed to recive responce.'}));  // If failed return an error.
+      }
+      else
+        this.setState({APIResponce: 'Please enter a username/password'});    // If failed return an error.
+    }
 
-  handleChange(changeObject) {                                           // Sets the data values of variables.
-    this.setState(changeObject)
-  }
+  componentWillMount()  {}                                                   // This is periodicly run.
   
   render() {
     
@@ -45,7 +45,7 @@ class App extends React.Component {
       <>
       <div className="login-box">
       <br/> <h1 type="title">Login</h1> <br/>
-      <form acceptCharset="UTF-8" onSubmit={this.Authenticate}>
+      <form acceptCharset="UTF-8" >
         <img src="images/user.png" width={100} height={100} />
         <input
           type="username"
@@ -53,8 +53,8 @@ class App extends React.Component {
           maxLength="24"
           name ="username"
           value={this.state.username}
-          onChange={(e) => this.handleChange({ username: e.target.value })}
-          
+          onChange={(e) => this.setState({ username: e.target.value })}
+          required
           />
         <input
           type="password"
@@ -62,12 +62,12 @@ class App extends React.Component {
           maxLength="24"
           name ="password"
           value={this.state.password}
-          onChange={(e) => this.handleChange({ password: e.target.value })}
-          
+          onChange={(e) => this.setState({ password: e.target.value })}
+          required
         />
         <div>
-          <button type="submit">Login</button>
-          <button type="submit">Register</button>
+          <button id="button1" onClick={this.Authenticate}>Login</button>
+          <button id="button1" onClick={this.Register}>Register</button>
         </div>
         <p>{this.state.APIResponce}</p>
         </form>
