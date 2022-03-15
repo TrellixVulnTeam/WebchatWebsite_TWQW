40d28a0ddf39ca0fdf14eb33049b01ed6f32ef67
Sam <swiseman2005@gmail.com>
2022-03-15 15:01:44 -0500

    add deafult redirect to /hub/login

--- client/src/index.js
+++ client/src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Home from './hub/home'
-import { BrowserRouter, Routes, Route, link } from 'react-router-dom';
-
+import { BrowserRouter, Routes, Route, link, Navigate } from 'react-router-dom';
 const routs = (
   <BrowserRouter>
     <Routes>
-        <Route exact path="/hub/login" element={<App/>} />
-        <Route exact path="hub/home" element={<Home/>}/>
+      <Route exact path="/" element={<Navigate to='/hub/login' />} />
+      <Route exact path="/hub/login" element={<App />} />
+      <Route exact path="hub/home" element={<Home />} />
     </Routes>
   </BrowserRouter>
 );
 
 ReactDOM.render(
-    routs,
+  routs,
   document.getElementById('root')
 );
