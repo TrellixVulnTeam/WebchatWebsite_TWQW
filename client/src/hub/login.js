eefb1002cf9d9f1a4665cf627377ba6463f0973d
Sam <swiseman2005@gmail.com>
2022-03-16 13:44:56 -0500

    add alphanumeric check to inputs

--- client/src/hub/login.js
+++ client/src/hub/login.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route, link, Navigate, Link } from 'react-router
 
 
 class App extends React.Component {
-  constructor(props)  {                                                     // Init our constructor and variables.
+  constructor(props) {                                                     // Init our constructor and variables.
     super(props);
     this.state = {
       apiResponce: '',
@@ -12,49 +12,58 @@ class App extends React.Component {
       password: ''
     };
     this.Authenticate = this.Authenticate.bind(this);                       // So we are able to call this from render()'s html.
-    this.Register = this.Register.bind(this); 
-    }
+    this.Register = this.Register.bind(this);
+  }
 
-    async Register(event) {                                                       // Todo: add a register function to the API
-      event.preventDefault();
-      let dataPOST =  {
+  async Register(event) {                                                       // Todo: add a register function to the API
+    event.preventDefault();
+    if (!this.state.username.match(/^[0-9a-zA-Z]+$/) || !this.state.password.match(/^[0-9a-zA-Z]+$/)) {
+      this.setState({ apiResponce: 'Please use alphanumeric chars' });
+    }
+    else {
+      let dataPOST = {
         username: this.state.username,
         password: this.state.password
       };
-      if(this.state.username && this.state.password)  {
+      if (this.state.username && this.state.password) {
         const response = await fetch('http://localhost:3000/api/v1/beta', {
           method: 'POST',
           body: JSON.stringify(dataPOST),
           headers: { 'Content-Type': 'application/json' }
         })
-        .then((res) => res.json())                                           // Parse responce as JSON.
-        .then((data) => this.setState({apiResponce: data.success}))          // Set apiResponce to data.TYPE <-- responce of the api/v1/login function.
-        .catch((error) => this.setState({apiResponce: 'Failed to recive responce.'}));  // If failed return an error.
+          .then((res) => res.json())                                           // Parse responce as JSON.
+          .then((data) => this.setState({ apiResponce: data.success }))          // Set apiResponce to data.TYPE <-- responce of the api/v1/login function.
+          .catch((error) => this.setState({ apiResponce: 'Failed to recive responce.' }));  // If failed return an error.
       }
       else
-        this.setState({apiResponce: 'Please enter a username/password'});    // If failed return an error.
+        this.setState({ apiResponce: 'Please enter a username/password' });    // If failed return an error.
     }
-
-    async Authenticate(event)  {
-      event.preventDefault();                                               // So we don't refresh page on function call.
-      let dataPOST =  {
+  }
+  async Authenticate(event) {
+    event.preventDefault();                                               // So we don't refresh page on function call.
+    if (!this.state.username.match(/^[0-9a-zA-Z]+$/) || !this.state.password.match(/^[0-9a-zA-Z]+$/)) {
+      this.setState({ apiResponce: 'Please use alphanumeric chars' });
+    }
+    else {
+      let dataPOST = {
         username: this.state.username,
         password: this.state.password
       };
-      if(this.state.username != '' && this.state.password != 0)  {
+      if (this.state.username != '' && this.state.password != 0) {
         const response = await fetch('http://localhost:3000/api/v1/login', {
           method: 'POST',
           body: JSON.stringify(dataPOST),
           headers: { 'Content-Type': 'application/json' }
         })
-        .then((res) => res.json())                                           // Parse responce as JSON.
-        .then((data) => this.setState({apiResponce: data.success}))          // Set apiResponce to data.TYPE <-- responce of the api/v1/login function.
-        .catch((error) => this.setState({apiResponce: 'Failed to recive responce.'}));  // If failed return an error.
+          .then((res) => res.json())                                           // Parse responce as JSON.
+          .then((data) => this.setState({ apiResponce: data.success }))          // Set apiResponce to data.TYPE <-- responce of the api/v1/login function.
+          .catch((error) => this.setState({ apiResponce: 'Failed to recive responce.' }));  // If failed return an error.
       }
       else
-        this.setState({apiResponce: 'Please enter a username/password'});    // If failed return an error.
+        this.setState({ apiResponce: 'Please enter a username/password' });    // If failed return an error.
+    }
   }
-  
+
   render() {
     /*
     if(this.state.apiResponce == "true")  {
@@ -63,35 +72,35 @@ class App extends React.Component {
 
     return (
       <>
-      <div class="login-box">
-      <br/> <h1 type="title">Login</h1> <br/>
-      <form acceptCharset="UTF-8">
-        <img src="/images/user.png" width={100} height={100} />
-        <input
-          type="username"
-          placeholder="Username"
-          maxLength="24"
-          name ="username"
-          value={this.state.username}
-          onChange={(e) => this.setState({ username: e.target.value })}
-          required
-          />
-        <input
-          type="password"
-          placeholder="Password"
-          maxLength="24"
-          name ="password"
-          value={this.state.password}
-          onChange={(e) => this.setState({ password: e.target.value })}
-          required
-        />
-        <div>
-          <button id="button1" onClick={this.Authenticate}>Login</button>
-          <button id="button1" onClick={this.Register}>Register</button>
+        <div class="login-box">
+          <br /> <h1 type="title">Login</h1> <br />
+          <form acceptCharset="UTF-8">
+            <img src="/images/user.png" width={100} height={100} />
+            <input
+              type="username"
+              placeholder="Username"
+              maxLength="24"
+              name="username"
+              value={this.state.username}
+              onChange={(e) => this.setState({ username: e.target.value })}
+              required
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              maxLength="24"
+              name="password"
+              value={this.state.password}
+              onChange={(e) => this.setState({ password: e.target.value })}
+              required
+            />
+            <div>
+              <button id="button1" onClick={this.Authenticate}>Login</button>
+              <button id="button1" onClick={this.Register}>Register</button>
+            </div>
+            <p>{this.state.apiResponce}</p>
+          </form>
         </div>
-        <p>{this.state.apiResponce}</p>
-        </form>
-      </div>
       </>
     );
   }
